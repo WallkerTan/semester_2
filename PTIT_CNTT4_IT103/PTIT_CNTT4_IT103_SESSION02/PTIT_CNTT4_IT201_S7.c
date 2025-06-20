8dcdb9b2885b3f6feceb7e622c2864c57f80d450
phamtan220106 <tanpham220106z@gmail.com>
2025-06-20 10:57:09 +0700

    session02

--- PTIT_CNTT4_IT103/PTIT_CNTT4_IT103_SESSION02/PTIT_CNTT4_IT201_S7.c
+++ PTIT_CNTT4_IT103/PTIT_CNTT4_IT103_SESSION02/PTIT_CNTT4_IT201_S7.c
@@ -15,18 +15,18 @@ int main()
     int sum;
     scanf("%d", &sum);
 
-    int map[MAX_value] = {0};
+    int newarr[MAX_value] = {0};
     for (int i = 0; i < n; i++)
     {
         int comp = sum - arr[i];
         int idx = comp + OFFSET;
-        if (idx >= 0 && idx <= MAX_value && map[idx] != 0)
+        if (idx >= 0 && idx <= MAX_value && newarr[idx] != 0)
         {
-            printf("vị trí: %d -> %d\nvị trí: %d -> %d\ntong: %d", map[idx] - 1, i, sum);
+            printf("vị trí: %d -> %d\nvị trí: %d -> %d\ntong: %d", newarr[idx] - 1, newarr[newarr[idx] - 1], i, newarr[i], sum);
             free(arr);
             return 0;
         }
-        map[arr[i] + OFFSET] = i + 1;
+        newarr[arr[i] + OFFSET] = i + 1;
     }
     printf("Khong tim thay cap nao co tong = %d\n", sum);
     free(arr);
